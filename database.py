7439a29ada9b24524cc5ea3c4f30f0dcd7ce259c
varun-program <varunyt.sai@outlook.com>
2026-03-15 22:31:16 +0530

    rwq

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+pymysql://root:ijGGIzdQtyDIdkIYWpwfvCpbiJiVSHYg@hopper.proxy.rlwy.net:40263/railway"
+DATABASE_URL = "mysql+pymysql://root:PASSWORD@hopper.proxy.rlwy.net:40263/railway"
 
 engine = create_engine(
     DATABASE_URL,
