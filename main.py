9e2c23e49d1a310ff6cf670ba38af5a3b1c63805
varun-program <varunyt.sai@outlook.com>
2026-03-15 22:33:07 +0530

    rwq

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
 from fastapi import FastAPI
-from routes import router
 from fastapi.middleware.cors import CORSMiddleware
+from routes import router
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],   # allow React
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 app.include_router(router)
-
-@app.get("/")
-def home():
-    return {"message":"Library System API"}
