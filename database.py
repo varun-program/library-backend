03dc8b71c211811695791c625fd97096310272a5
Varun <111887199+varun-program@users.noreply.github.com>
2026-03-16 10:04:42 +0530

    Refactor database connection URL
    Updated DATABASE_URL to use placeholders for credentials.

--- database.py
+++ database.py
@@ -1,12 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+pymysql://root:PASSWORD@hopper.proxy.rlwy.net:40263/railway"
+DATABASE_URL = "mysql+pymysql://USERNAME:PASSWORD@HOST:PORT/DATABASE"
 
-engine = create_engine(
-    DATABASE_URL,
-    pool_pre_ping=True
-)
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
     autocommit=False,
